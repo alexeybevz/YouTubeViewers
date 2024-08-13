1233c5e3c1c59feacaf0d8c40d6630e6e57dc7d4
alexeybevz <bevz.alexei@yandex.ru>
2024-08-13 11:13:28 +0500

    project setup

--- YouTubeViewers.WPF/App.xaml.cs
+++ YouTubeViewers.WPF/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace YouTubeViewers.WPF
 {
@@ -13,5 +7,12 @@ namespace YouTubeViewers.WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
