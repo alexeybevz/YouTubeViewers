1affb1e3e9efaffa07eb3111314241caed1344c9
alexeybevz <bevz.alexei@yandex.ru>
2024-08-19 11:04:20 +0500

    entity framework setup

---
+++ YouTubeViewers.EntityFramework/DTOs/YouTubeViewerDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YouTubeViewers.EntityFramework.DTOs
+{
+    public class YouTubeViewerDto
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public bool IsSubscribed { get; }
+        public bool IsMember { get; }
+    }
+}
